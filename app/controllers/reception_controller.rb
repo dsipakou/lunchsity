fababb0ed18b5944b84b2f6f5bd41ed7c0aafcfd
ror <ror@ror-virtual-machine.(none)>
2014-02-14 01:02:30 +0300

    new

--- app/controllers/reception_controller.rb
+++ app/controllers/reception_controller.rb
@@ -2,7 +2,7 @@ class ReceptionController < ApplicationController
 	layout 'test'
   def index
   	@title = "Фуршеты"
-	@recept = Reception.first
+	@reception = Reception.first
   	if Dir.glob(File.join('public', 'uploads', 'recept', "*.xls")).size == 1
   		@file = Dir.glob(File.join('public', 'uploads', 'recept', "*.xls"))
   	elsif Dir.glob(File.join('public', 'uploads', 'recept', "*.doc")).size == 1
